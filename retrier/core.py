96f6496184534428dbd8efe0b70b0c90b54a4299
keith <WindSoilder@outlook.com>
2017-09-21 02:13:39 -0700

    add situation for user input an website without a scheme

--- retrier/core.py
+++ retrier/core.py
@@ -1,5 +1,6 @@
 import time
 import logging
+from urllib import parse
 
 import requests
 from requests.exceptions import ConnectionError, Timeout, HTTPError
@@ -15,6 +16,7 @@ def try_fetch_until_success(url: str, retry_times: int=0) -> bool:
                      .format(retry=MAX_RETRY_TIMES))
         return False
     try:
+        url = ensure_scheme(url)
         response = requests.get(url)
         response.raise_for_status()
         return True
@@ -31,3 +33,14 @@ def is_server_side_error(status_code: int) -> bool:
     ''' check if the status code indicate the error is on server side '''
     checked_error_code = set([500, 501, 502, 503, 504])
     return status_code in checked_error_code
+
+
+def ensure_scheme(url: str) -> str:
+    ''' ensure the url contains scheme '''
+    parsed = parse.urlparse(url)
+    if parsed.scheme == "":
+        # add default scheme(http) to url
+        final_url = "".join(["http://", url])
+    else:
+        final_url = url
+    return final_url
--- tests/core.py
+++ tests/core.py
@@ -11,6 +11,11 @@ class CoreFunctionTest(unittest.TestCase):
         email = 'test@email.com'
         self.assertTrue(retrier.auto_retry(url, email))
 
+    def test_auto_retry_when_lack_schema(self):
+        url = 'www.bing.com'
+        email = 'test@email.com'
+        self.assertTrue(retrier.auto_retry(url, email))
+
     def test_is_server_side_error(self):
         expect_err_codes = [500, 501, 502, 503, 504]
         for err_code in expect_err_codes:
